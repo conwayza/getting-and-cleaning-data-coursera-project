0b7d2070cb18bc3aeca29504caba3bb02f8c5440
conwayza <77240614+conwayza@users.noreply.github.com>
2021-01-19 11:10:26 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,35 +1,35 @@
-library(reshape2)
+install.packages("reshape")
+library(reshape)
 library(tidyr)
 library(dplyr)
 
-filename <- "getdata_projectfiles_UCI HAR Dataset.zip"
+#Loading information
+volActiveLabels <- read.table("UCI Har Dataset/readme/activity_labels.txt")
+volFeatures <- read.table("UCI Har Dataset/readme/features.txt")
 
-if (!file.exists("UCI Har Dataset"){
-  unzip(filename)
-}
-
-volActiveLabels <- read.table("UCI Har Dataset//activity_labels.txt")
-volFeatures <- read.table("UCI Har Dataset//features.txt")
-
-volFeaturesNeeded <- grep(".mean.*|.std.*, volFeatures)
+#Extracting data and mean
+volFeaturesNeeded <- grep(".mean.*|.std.*", volFeatures)
 volFeaturesNeeded = gsub('-mean', 'Mean', volFeaturesNeeded)
 volFeaturesNeeded = gsub('-std', 'STD', volFeaturesNeeded)
 
-train <- read.table("UCI HAR Dataset//train//X_train.txt")
-trainActivities <- read.table("UCI HAR Dataset//train//Y_train.txt")
-trainSubjects <- read.table(UCI HAR Dataset//train//subject_train.txt")
+#Loading both datasets
+train <- read.table("UCI HAR Dataset/train/X_train.txt")
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
 train <- cbind(trainSubjects, trainActivities, train)
 
-test <- read.table("UCI HAR Dataset//test//X_test.txt")
-testActivites <- read.table("UCI HAR Dataset//test//Y_test.txt")
-testSubjects <- read.table("UCI HAR Dataset//test//subject_test.txt")
+test <- read.table("UCI HAR Dataset/test/X_test.txt")
+testActivites <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(testActivites, testSubjects, test)
 
+#Combining the sets
 combinedSets <- rbind(train, test)
-colnames(combinedSets) <- c("Subject", Activity", volFeaturesNeeded)
+colnames(combinedSets) <- c("Subject", "Activity", volFeaturesNeeded)
+
+#Factoring the sets
 combinedSets$Subject <- as.factor(combinedSets$Subject)
 combinedSets$Activity <- as.factor(combinedSets$Activity)
-combinedSets.melted <- melt(combinedSets, id = c("Subject", "Activity"))
-combinedSets.mean <- dcast(combinedSets.melted, Subject+Activity ~ variable, mean)
 
-write.table(combinedSets.mean, "tidy.txt", row.names=FALSE, quote=FALSE)
+#Writing to the tidy.txt file
+write.table(combinedSets, "tidy.txt", row.names=FALSE, quote=FALSE)
